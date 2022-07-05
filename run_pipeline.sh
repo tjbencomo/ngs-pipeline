7dcca89bc6cd276ad0777a88613bdb510f192ef7
Tomas Bencomo <tjbencomo@gmail.com>
2022-07-04 18:40:21 -0700

    run_pipeline launches from your current directory

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -10,7 +10,7 @@
 
 
 set -e
-cd $1
+cd $(pwd)
 snakemake --cluster-config cluster.wes.json -j 499 \
     --use-singularity \
     --cluster 'sbatch -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} -c {cluster.ncpus} -o {cluster.out}'
